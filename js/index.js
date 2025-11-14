de8cdd8a9152f823e1ce248fbf65a31595d2b981
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2025-11-14 11:31:54 +0100

    la till jest till några scenarion

--- js/index.js
+++ js/index.js
@@ -1,92 +1,113 @@
-// //Variables to use and it's a mix of global and local variables
 // let shoppingCart = [];
-// let products = document.getElementsByTagName("button");
 
-// let cartProducts = "";
+// let products = document.getElementsByClassName("button");
 
-// //This code gets whatever is inside data-product in the HTML
-// let product = event.target.parentNode.getAttribute("data-product");
-// cartProducts =
-//   cartProducts +
-//   '<li><span class="product-title">Titel: </span>' +
-//   shoppingCart[i] +
-//   "</li>";
-// shoppingCart.push(product);
+// function updateCart() {
+//   document.querySelector("#productsInCart").innerText = shoppingCart.length;
+// }
 
-// //
-// document.querySelector("#productsInCart").innerHTML = shoppingCart.length;
-// document.querySelector("#cart").classList.toggle("hide");
-// document.querySelector("#products").innerHTML = cartProducts;
-// products[i].addEventListener("click", (event) => {
-//   console.log(event.target);
-//   // event here is the HTML element that was clicked on
+// function listProductsInCart() {
+//   let cartProducts = "";
+//   for (let i = 0; i < shoppingCart.length; i++) {
+//     cartProducts += `
+//       <li>
+//         <span class="product-title">Titel: </span>${shoppingCart[i]}
+//         <button class="remove-btn" data-index="${i}">Ta bort</button>
+//       </li>
+//     `;
+//   }
+//   document.querySelector("#products").innerHTML = cartProducts;
+
+//   // Lägg på event listeners på de nya remove-knapparna
+//   let removeBtn = document.getElementsByClassName("remove-btn");
+//   for (let i = 0; i < removeBtn.length; i++) {
+//     removeBtn[i].addEventListener("click", remove);
+//   }
+// }
+
+// // Loop through all buttons
+// for (let i = 0; i < products.length; i++) {
+//   products[i].addEventListener("click", (event) => {
+//     let product = event.target.parentNode.getAttribute("data-product");
+//     if (shoppingCart.includes(product)) {
+//       console.log("finns");
+//     } else {
+//       shoppingCart.push(product);
+//     }
+
+//     updateCart();
+//     listProductsInCart();
+//   });
+// }
+
+// // Toggle cart visibility
+// document.querySelector("#open-cart").addEventListener("click", () => {
+//   document.querySelector("#cart").classList.toggle("hide");
 // });
-// document.querySelector("#open-cart").addEventListener("click", () => {});
 
-// //Function declarations, add code inside {}
-// function updateCart() {}
-// function listProductsInCart() {}
+// function remove(event) {
+//   let index = event.target.getAttribute("data-index"); // index i shoppingCart
+//   shoppingCart.splice(index, 1); // tar bort produkten från arrayen
+//   updateCart(); // uppdatera antal i kundvagnen
+//   listProductsInCart(); // uppdatera listan med produkter
+// }
 
-// //For-loops of two arrays
-// for (let i = 0; i < shoppingCart.length; i++) {}
-// for (let i = 0; i < products.length; i++) {}
+document.addEventListener("DOMContentLoaded", () => {
+  let shoppingCart = [];
 
-// //Function calls and there should be two listProductsInCart()
-// updateCart();
-// listProductsInCart();
-// listProductsInCart();
+  // Hämta alla "Add to cart"-knappar
+  let products = document.getElementsByClassName("button");
 
-let shoppingCart = [];
-
-let products = document.getElementsByClassName("button");
-let removeBtn = document.getElementsByClassName("remove-btn");
-
-function updateCart() {
-  document.querySelector("#productsInCart").innerText = shoppingCart.length;
-}
-
-function listProductsInCart() {
-  let cartProducts = "";
-  for (let i = 0; i < shoppingCart.length; i++) {
-    cartProducts += `
-      <li>
-        <span class="product-title">Titel: </span>${shoppingCart[i]}
-        <button class="remove-btn" data-index="${i}">Ta bort</button>
-      </li>
-    `;
+  // Uppdatera antal produkter i kundvagnen
+  function updateCart() {
+    document.querySelector("#productsInCart").textContent = shoppingCart.length;
   }
-  document.querySelector("#products").innerHTML = cartProducts;
 
-  // Lägg på event listeners på de nya remove-knapparna
-  let removeBtn = document.getElementsByClassName("remove-btn");
-  for (let i = 0; i < removeBtn.length; i++) {
-    removeBtn[i].addEventListener("click", remove);
-  }
-}
-
-// Loop through all buttons
-for (let i = 0; i < products.length; i++) {
-  products[i].addEventListener("click", (event) => {
-    let product = event.target.parentNode.getAttribute("data-product");
-    if (shoppingCart.includes(product)) {
-      console.log("finns");
-    } else {
-      shoppingCart.push(product);
+  // Lista produkter i kundvagnen
+  function listProductsInCart() {
+    let cartProducts = "";
+    for (let i = 0; i < shoppingCart.length; i++) {
+      cartProducts += `
+        <li>
+          <span class="product-title">Titel: </span>${shoppingCart[i]}
+          <button class="remove-btn" data-index="${i}">Ta bort</button>
+        </li>
+      `;
     }
+    document.querySelector("#products").innerHTML = cartProducts;
 
+    // Lägg event listeners på de nya remove-knapparna
+    let removeBtns = document.getElementsByClassName("remove-btn");
+    for (let i = 0; i < removeBtns.length; i++) {
+      removeBtns[i].addEventListener("click", remove);
+    }
+  }
+
+  // Lägg till produkt när knappen klickas
+  for (let i = 0; i < products.length; i++) {
+    products[i].addEventListener("click", (event) => {
+      // Hitta närmaste artikel med data-product
+      let product = event.target.closest(".card").getAttribute("data-product");
+
+      if (!shoppingCart.includes(product)) {
+        shoppingCart.push(product);
+      }
+
+      updateCart();
+      listProductsInCart();
+    });
+  }
+
+  // Toggle kundvagnens synlighet
+  document.querySelector("#open-cart").addEventListener("click", () => {
+    document.querySelector("#cart").classList.toggle("hide");
+  });
+
+  // Ta bort produkt från kundvagnen
+  function remove(event) {
+    let index = event.target.getAttribute("data-index");
+    shoppingCart.splice(index, 1);
     updateCart();
     listProductsInCart();
-  });
-}
-
-// Toggle cart visibility
-document.querySelector("#open-cart").addEventListener("click", () => {
-  document.querySelector("#cart").classList.toggle("hide");
+  }
 });
-
-function remove(event) {
-  let index = event.target.getAttribute("data-index"); // index i shoppingCart
-  shoppingCart.splice(index, 1); // tar bort produkten från arrayen
-  updateCart(); // uppdatera antal i kundvagnen
-  listProductsInCart(); // uppdatera listan med produkter
-}
---
+++ js/index.test.js
@@ -0,0 +1,124 @@
+require("@testing-library/jest-dom");
+const { fireEvent } = require("@testing-library/dom");
+
+const html = `  <body>
+    <header class="header">
+      <nav class="menu">
+        <a href="#" class="link" id="open-cart">Cart</a>
+        <span class="cart-total" id="productsInCart">0</span>
+        <section class="cart-wrapper hide" id="cart">
+          <h3>Tillagda produkter</h3>
+          <ul id="products"></ul>
+        </section>
+      </nav>
+    </header>
+    <main>
+      <section class="cards">
+        <article class="card" data-product="Goodnight Moon">
+          <h3 class="title">Goodnight Moon</h3>
+          <p class="author">Av Margaret Wise Brown</p>
+          <p class="about">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil odio
+            unde ad modi amet deleniti explicabo debitis earum rem vel illum
+            dolores, quidem quibusdam adipisci dolorem repellendus repellat
+            mollitia! Voluptatem?
+          </p>
+          <button class="button" id="add-to-cart-button">Add to cart</button>
+        </article>
+        <article class="card" data-product="The Very Hungry Caterpillar">
+          <h3 class="title">The Very Hungry Caterpillar</h3>
+          <p class="author">Av Eric Carle</p>
+          <p class="about">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil odio
+            unde ad modi amet deleniti explicabo debitis earum rem vel illum
+            dolores, quidem quibusdam adipisci dolorem repellendus repellat
+            mollitia! Voluptatem?
+          </p>
+          <button class="button">Add to cart</button>
+        </article>
+        <article class="card" data-product="A Wrinkle in Time">
+          <h3 class="title">A Wrinkle in Time</h3>
+          <p class="author">Av Madeleine L’Engle</p>
+          <p></p>
+          <p class="about">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil odio
+            unde ad modi amet deleniti explicabo debitis earum rem vel illum
+            dolores, quidem quibusdam adipisci dolorem repellendus repellat
+            mollitia! Voluptatem?
+          </p>
+          <button class="button">Add to cart</button>
+        </article>
+        <article class="card" data-product="Where the Wild Things Are">
+          <h3 class="title">Where the Wild Things Are</h3>
+          <p class="author">Av Maurice Sendak</p>
+          <p></p>
+          <p class="about">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil odio
+            unde ad modi amet deleniti explicabo debitis earum rem vel illum
+            dolores, quidem quibusdam adipisci dolorem repellendus repellat
+            mollitia! Voluptatem?
+          </p>
+          <button class="button">Add to cart</button>
+        </article>
+      </section>
+    </main>
+    <script src="js/index.js"></script>
+  </body>`;
+// Allt sker i den fejkade jsdom-DOM, men med riktig JS-logik från index.js.
+
+describe("functionality so the app works", () => {
+  beforeEach(() => {
+    document.body.innerHTML = html;
+    jest.resetModules(); // Återställar DOM och JS-skript inför varje test
+    //När Node kör require laddas din riktiga JS-kod (index.js) och körs.
+    require("./index.js");
+    // Triggera DOMContentLoaded manuellt
+    const event = new Event("DOMContentLoaded");
+    document.dispatchEvent(event);
+  });
+  it("should work to add a book when the button is clicked", () => {
+    //arrange
+    // hämtar alla knappar etc som vanlig JS
+    const buttons = document.querySelectorAll(".button");
+    const cartCounter = document.getElementById("productsInCart");
+    const productsList = document.getElementById("products");
+    //act
+    fireEvent.click(buttons[0]);
+    //assert
+    expect(cartCounter).toHaveTextContent("1");
+    expect(productsList).toHaveTextContent("Goodnight Moon");
+  });
+
+  it("should not work to add same book again", () => {
+    // arrange
+    const buttons = document.querySelectorAll(".button");
+    const cartCounter = document.getElementById("productsInCart");
+    const productsList = document.getElementById("products");
+
+    // act
+    fireEvent.click(buttons[0]); // lägg till första gången
+    fireEvent.click(buttons[0]); // försök lägga till samma bok igen
+
+    // assert
+    expect(cartCounter).toHaveTextContent("1"); // antal ska fortfarande vara 1
+    expect(productsList).toHaveTextContent("Goodnight Moon"); // samma bok ska visas bara en gång
+  });
+  it("should be possible to delete a book", () => {
+    const cartCounter = document.getElementById("productsInCart");
+    const productsList = document.getElementById("products");
+    const buttons = document.querySelectorAll(".button");
+    // Lägg till en bok först
+    fireEvent.click(buttons[0]);
+
+    // Nu genereras remove-knappen genom din listProductsInCart-funktion
+    const removeButtons = document.querySelectorAll(".remove-btn");
+    expect(removeButtons.length).toBe(1); // säkerställ att knappen finns
+
+    // Klicka på “Ta bort”-knappen
+    fireEvent.click(removeButtons[0]);
+
+    // Kolla att varukorgen är tom och att listan är tom
+    expect(cartCounter).toHaveTextContent("0");
+    expect(productsList).toHaveTextContent("");
+  });
+});
